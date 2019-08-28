fbf40f3446b28fb6e5954466442eba026fac9d93
NexusReflex <laura.bielenberg@tum.de>
2019-08-28 18:50:23 +0200

    changed ip and network

--- esp/m3_control/src/main.c
+++ esp/m3_control/src/main.c
@@ -65,7 +65,7 @@ static const adc_unit_t unit = ADC_UNIT_1;
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
 #define EXAMPLE_ESP_WIFI_SSID      "laura-MacBookPro"
-#define EXAMPLE_ESP_WIFI_PASS      "laura-MacBookPro"
+#define EXAMPLE_ESP_WIFI_PASS      "pAIdHARF"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  10
 
 /* FreeRTOS event group to signal when we are connected*/
